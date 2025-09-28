4901f5d9e228d539797e948f97e6966e5be1e61a
Troptop <87533146+NickIsADev@users.noreply.github.com>
2025-09-28 15:07:00 -0400

    bro tryna be different :skull::wilted_rose:

--- init.lua
+++ init.lua
@@ -280,6 +280,16 @@ function ropi:request(api, method, endpoint, headers, body, _, version)
 	end
 end
 
+coroutine.wrap(function()
+	while true do
+		if #ropi.Requests > 0 then
+			ropi:dump()
+		end
+
+		timer.sleep(5)
+	end
+end)()
+
 -- api functions
 
 function ropi.SetCookie(token)
@@ -562,12 +572,5 @@ function ropi.SetAssetPrice(collectibleID, price)
 		return false, result
 	end
 end
-
-local dumpTimer = uv.new_timer()
-uv.timer_start(dumpTimer, 0, 100, function()
-	if next(ropi.Requests) then
-		ropi:dump()
-	end
-end)
 
 return ropi
